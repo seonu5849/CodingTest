0744be74d682826a6a4a3a856afbe81c3ea9c599
Seonoo <tjsdn22@naver.com>
2024-01-11 00:51:06 +0900

    refactor: 없는 숫자 더하기, 람다로 리펙토링

--- 프로그래머스/Level_1/없는 숫자 더하기/SumOfMissingNumbers.java
+++ 프로그래머스/Level_1/없는 숫자 더하기/SumOfMissingNumbers.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SumOfMissingNumbers {
 
@@ -16,7 +17,7 @@ public class SumOfMissingNumbers {
     }
 
     public int solution(int[] numbers) {
-        int answer = 0;
+        /*int answer = 0;
 
         for(int i=0; i<=9; i++){
             boolean found = false;
@@ -31,7 +32,16 @@ public class SumOfMissingNumbers {
             if(!found){
                 answer+=i;
             }
-        }
+        }*/
+
+        /*
+        * 리펙토링 - 람다
+        * rangeClosed()은 IntStream 인터페이스에서 제공하는 메서드 중 하나, 시작값과 종료값을 포함한 범위의 정수 스트림 생성
+        * noneMatch() 메서드는 stream 인터페이스에서 제공하는 메소드 중 하나, stream의 값과 비교하는 값(num)이 다르다면 true, 같다면 false를 출력한다.
+        */
+        int answer = IntStream.rangeClosed(0,9)
+                .filter(num -> Arrays.stream(numbers).noneMatch(value -> num == value))
+                .sum();
 
         return answer;
     }
