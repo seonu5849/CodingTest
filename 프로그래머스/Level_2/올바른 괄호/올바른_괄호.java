8d377ac480f22b012ee7fd055b7ec222c07da00f
Seonoo <tjsdn22@naver.com>
2024-01-21 11:50:47 +0900

    refactor: 프로그래머스 레벨 2 - 올바른 괄호 리펙토링

--- 프로그래머스/Level_2/올바른 괄호/올바른_괄호.java
+++ 프로그래머스/Level_2/올바른 괄호/올바른_괄호.java
@@ -5,21 +5,20 @@ import java.util.Stack;
 public class 올바른_괄호 {
     public static void main(String[] args) {
 //        String s = "()()";
-        String s = "(()()";
+        String s = "(((())))()";
         boolean result = solution(s);
         System.out.println("result = " + result); // true
     }
 
     static boolean solution(String s) {
-        String[] arr = s.split("");
-        Stack<String> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
 
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i<s.length(); i++){
             if(stack.isEmpty()){
-                stack.push(arr[i]);
+                stack.push(s.charAt(i));
             }else{
-                if(arr[i].equals("(")){
-                    stack.push(arr[i]);
+                if(s.charAt(i) == '('){
+                    stack.push(s.charAt(i));
                 }else{
                     stack.pop();
                 }
